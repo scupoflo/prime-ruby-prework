5be1124d59e5f8db200d4ae9109d69d966fcc35a
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-15 23:56:30 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(number)
+
+#   (2..number - 1).each do |i|
+#     if (number % i) == 0 && number != i &&  number != 1 && number > 1
+#       return false
+#     else
+#       return true
+#     end
+#   end
+
+  if number > 1
+    (2..number - 1).none? do |num|
+      number % num == 0
+    end
+  else
+    return false
+  end
+
+end
